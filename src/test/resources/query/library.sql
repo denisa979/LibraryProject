4a7f124d2d002321a4f4eaba04b3473b74746992
denisa <dmutapcic79@hotmail.com>
2024-11-21 21:50:05 -0500

    Testing is done

--- src/test/resources/query/library.sql
+++ src/test/resources/query/library.sql
@@ -6,8 +6,8 @@ select * from book_borrow
 where is_returned = 0;
 
 select * from users
-where id=14815;
+where id=1;
 
 select full_name,email,user_group_id,status,start_date,end_date,address
 from users
-where id=14815;
+where id=18635;
